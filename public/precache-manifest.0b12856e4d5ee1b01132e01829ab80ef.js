908d7a3eb51edd221f502b582dde9d43fba5a8da
Tianyu Li <tianyuli@fb.com>
2020-05-25 00:22:12 -0700

    Experiment with public folder

---
+++ public/precache-manifest.0b12856e4d5ee1b01132e01829ab80ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1205baa9dba2b210a2de3f041f0f9c6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4612b37463dc80b85d1e",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe366b2f14445a13431d",
+    "url": "/static/js/2.e784cc32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e784cc32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4612b37463dc80b85d1e",
+    "url": "/static/js/main.4016d97f.chunk.js"
+  },
+  {
+    "revision": "cff1a083b6f1ccd127ae",
+    "url": "/static/js/runtime-main.b21d095f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
